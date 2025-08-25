898d027dbe27e76ae1b265cf59ceff864b644a9c
daxho <developer.daxho@gmail.com>
2025-08-25 16:39:33 -0400

    updated styles

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,7 @@
 export default function Features({ children }) {
   return (
-    <div className="w-[min(95vw,75rem)] px-6 grid gap-8 grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 items-center grid-flow-row lg:grid-flow-col justify-items-center">
+    <main className="w-[min(95vw,75rem)] px-6 grid gap-8 grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 items-center grid-flow-row lg:grid-flow-col justify-items-center">
       {children}
-    </div>
+    </main>
   );
 }
